d4d5e17d9175296ece8d5751f881ee11edcb7066
M.Abdullah Gürsu <1205012005@ybu.edu.tr>
2015-12-04 01:54:41 +0200

    Create Shipment.java

---
+++ Artificial-Intelligence/HW1/Shipment.java
@@ -0,0 +1,25 @@
+
+/**
+ *
+ * @author MuhammedAbdullah
+ */
+public class Shipment {
+    private String shipmentLoc;
+    private String destination;
+
+    public String getShipmentLoc() {
+        return shipmentLoc;
+    }
+
+    public void setShipmentLoc(String shipmentLoc) {
+        this.shipmentLoc = shipmentLoc;
+    }
+    
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+}
